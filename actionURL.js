d3fcf8a7c960be498d0d7f32834253dc3049a5f7
Phurich <phurich2002@hotmail.com>
2024-08-09 09:32:04 +0700

    Update currentActionURL

--- actionURL.js
+++ actionURL.js
@@ -1,2 +1,2 @@
 var testActionUrl = "https://script.google.com/macros/s/AKfycbyzL2u8HZ8eviN4Ma7qyks0AfEGXnwStwddbgwDQ5Ds/dev";
-var currentActionUrl = "https://script.google.com/macros/s/AKfycbzzPUYstKZeyzYVICU6CVDsh2voxjEJYm2LkI_OintvBgJJmyuw4eTYJNV7ZyDNQvl7vQ/exec";
+var currentActionUrl = "https://script.google.com/macros/s/AKfycby0t7bHa_epbnppHcTY7Qjciv2uG0_fhxTXqFqliHek_Qv_aje14ABjDnNQMBNQcdxDnA/exec";
