df7bbcec0c21195fe97c2472b9539ddd751d0699
Phurich <phurich2002@hotmail.com>
2025-01-06 01:28:03 +0700

    Add files via upload

--- library/utils.js
+++ library/utils.js
@@ -6,13 +6,13 @@ class myHtmlHeader extends HTMLElement {
         const headContent = `
         <!-- common-head.html -->
         <title>ภูธรเดลิเวอรี่</title>
-        <link rel="icon" type="image/png" href="./library/favicon.png">
+        <link rel="icon" type="image/png" href="https://phurich.github.io/phuthon/library/favicon.png">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Kanit&display=swap">
-        <link rel="stylesheet" href="./library/style.css">
+        <link rel="stylesheet" href="https://phurich.github.io/phuthon/library/style.css">
         `;
 
         document.head.insertAdjacentHTML('afterbegin', headContent);
