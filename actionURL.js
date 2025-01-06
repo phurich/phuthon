e1380c61ac314eac66d2b21da7a66a93876d9480
Phurich <phurich2002@hotmail.com>
2025-01-07 06:15:33 +0700

    Delete actionURL.js
    rename to util.js and move it to library folder

--- actionURL.js
+++
@@ -1,2 +0,0 @@
-var testActionUrl = "https://script.google.com/macros/s/AKfycbyzL2u8HZ8eviN4Ma7qyks0AfEGXnwStwddbgwDQ5Ds/dev";
-var currentActionUrl = "https://script.google.com/macros/s/AKfycbzD2OiC5tzTfdgxOjKOPZFKSOmqJE_XE2-iJKHE65jSTH0zcE2ZzNgSKc-w3S8z9BSRsw/exec";
